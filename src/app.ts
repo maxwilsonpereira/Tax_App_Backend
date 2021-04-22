07a3ceb4de77c2c7f9c950a58a130ef6632a24ff
“maxwilsonpereira <maxwilsonpereira@gmail.com>
2021-04-22 15:05:34 +0200

    procfile added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 // *** RUN THE SERVER with auto compilation from TS file to JS with "watch mode"
 // TERMINAL 1: tsc -w / TERMINAL 2: nodemon
 
-import express, { Request, Response, NextFunction } from "express";
-import { json } from "body-parser";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import fs from "fs"; // file system (read, create, uodate, delete, rename files)
-import path from "path"; // provides a way of working with directories and file paths
-import userRouter from "./routes/users";
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+// https://www.npmjs.com/package/cors
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import fs from 'fs'; // file system (read, create, uodate, delete, rename files)
+import path from 'path'; // provides a way of working with directories and file paths
+import userRouter from './routes/users';
 
 const app = express();
 app.use(json());
@@ -18,37 +19,37 @@ dotenv.config();
 
 // saving login infos with morgan
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
+  path.join(__dirname, 'access.log'),
   // flags: "a" means that NEW data will be added
-  { flags: "a" }
+  { flags: 'a' }
 );
-app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(helmet()); // middleware that will add security headers
 
 // options for cors midddleware
 const options: cors.CorsOptions = {
   allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "X-Access-Token",
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
     // "Access-Control-Allow-Origin",
     // "ssid",
     // Authorization MUST BE ENABLED TO USE AUTHENTICATION:
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization",
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization',
   ],
   credentials: true,
-  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: "*", // DEV MODE
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*', // DEV MODE
   //   origin: API_URL, // PRODUCTION MODE
   preflightContinue: false,
 };
 app.use(cors(options));
 // app.options("*", cors(options)); // enable pre-flight
 
-app.use("/users", userRouter);
+app.use('/users', userRouter);
 
 // ERROR HANDLER MIDDLEWARE will be fired if ANY ERROR occurs
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -69,6 +70,6 @@ mongoose
   })
   .then((result) => {
     app.listen(process.env.PORT || 8080);
-    console.log("API simulator running at port 8080");
+    console.log('API simulator running at port 8080');
   })
   .catch((err) => console.log(err));
